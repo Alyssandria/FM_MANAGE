bb131c73c4384697a41ec6cf7d5a4cc86f60bb96
Alyssandria <sanchez.roniel.work@gmail.com>
2025-10-31 21:41:17 +0800

    feat: text constants

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,75 +1,127 @@
-Pricing
-Product
-  About Us
-Careers
-Community
+export const NAVLINKS = {
+  PRIMARY: [
+    {
+      NAME: "HOME",
+      CONTENT: "Home",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: false,
+    },
+    {
+      NAME: "PRICING",
+      CONTENT: "Pricing",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "PRODUCT",
+      CONTENT: "Product",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "ABOUT_ US",
+      CONTENT: "About us",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "CAREERS",
+      CONTENT: "Careers",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "COMMUNITY",
+      CONTENT: "Community",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "CAREERS",
+      CONTENT: "Careers",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "COMMUNITY",
+      CONTENT: "Community",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    },
+    {
+      NAME: "PRIVACY_POLICY",
+      CONTENT: "Privacy Policy",
+      CLASSNAME: "",
+      INFOOTER: true,
+      INHEADER: true,
+    }
+  ],
+}
 
-  Get Started
-
-  Bring everyone together to build better products.
-
-  Manage makes it simple for software teams to plan day - to - day 
-  tasks while keeping the larger team goals in view.
-
-  Get Started
-
-What’s different about Manage ?
-
-  Manage provides all the functionality your team needs, without 
-  the complexity.Our software is tailor - made for modern digital 
-  product teams. 
-
-  01
-  Track company - wide progress
-  See how your day - to - day tasks fit into the wider vision.Go from 
-  tracking progress at the milestone level all the way done to the 
-  smallest of details.Never lose sight of the bigger picture again.
-
-  02
-  Advanced built -in reports
-  Set internal delivery estimates and track progress toward company
-goals.Our customisable dashboard helps you build out the reports 
-  you need to keep key stakeholders informed.
-
-  03
-  Everything you need in one place
-  Stop jumping from one service to another to communicate, store files,
-  track tasks and share documents.Manage offers an all -in -one team 
-  productivity solution.
-
-  What they’ve said
-
-  Anisha Li
-  “Manage has supercharged our team’s workflow.The ability to maintain 
-  visibility on larger milestones at all times keeps everyone motivated.”
-
-  Ali Bravo
-  “We have been able to cancel so many other subscriptions since using
-Manage.There is no more cross - channel confusion and everyone is much 
-  more focused.”
-
-  Richard Watts
-  “Manage allows us to provide structure and process.It keeps us organized 
-  and focused.I can’t stop recommending them to everyone I talk to!”
-
-  Shanai Gough
-  “Their software allows us to track, manage and collaborate on our projects 
-  from anywhere.It keeps the whole team in -sync without being intrusive.”
-
-  Get Started
-
-  Simplify how your team works today.
-  Get Started
-
-Home
-Pricing
-Products
-  About Us
-Careers
-Community
-  Privacy Policy
-
-Updates in your inbox…
-Go
-
-  Copyright 2020. All Rights Reserved
+export const MAIN = {
+  HERO: {
+    TAGLINE: "Bring everyone together to build better products.",
+    SUBTITLE: "Manage makes it simple for software teams to plan day - to - day tasks while keeping the larger team goals in view."
+  },
+  FEATURES: {
+    TAGLINE: "What’s different about Manage?",
+    SUBTITLE: "Manage provides all the functionality your team needs, without the complexity.Our software is tailor - made for modern digital product teams.",
+    CARDS: [
+      {
+        STEP: "01",
+        TITLE: "Track company-wide progress",
+        CONTENT: "See how your day - to - day tasks fit into the wider vision.Go from tracking progress at the milestone level all the way done to the smallest of details.Never lose sight of the bigger picture again."
+      },
+      {
+        STEP: "02",
+        TITLE: "Advanced built-in reports",
+        CONTENT: "Set internal delivery estimates and track progress toward company goals.Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed."
+      },
+      {
+        STEP: "03",
+        TITLE: "Everything you need in one place",
+        CONTENT: "Stop jumping from one service to another to communicate, store files, track tasks and share documents.Manage offers an all -in -one team productivity solution."
+      },
+    ]
+  },
+  TESTIMONIALS: {
+    TAGLINE: "What they’ve said",
+    CARDS: [
+      {
+        NAME: "Anisha Li",
+        CONTENT: "“Manage has supercharged our team’s workflow.The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+        AVATAR: "",
+        CLASSNAME: ""
+      },
+      {
+        NAME: "Ali Bravo",
+        CONTENT: "“We have been able to cancel so many other subscriptions since using Manage.There is no more cross - channel confusion and everyone is much more focused.”",
+        AVATAR: "",
+        CLASSNAME: ""
+      },
+      {
+        NAME: "Richard Watts",
+        CONTENT: "“Manage allows us to provide structure and process.It keeps us organized and focused.I can’t stop recommending them to everyone I talk to!”",
+        AVATAR: "",
+        CLASSNAME: ""
+      },
+      {
+        NAME: "Shanai Gough",
+        CONTENT: "“Their software allows us to track, manage and collaborate on our projects from anywhere.It keeps the whole team in -sync without being intrusive.”",
+        AVATAR: "",
+        CLASSNAME: ""
+      },
+    ]
+  },
+  MARKETING: {
+    TAGLINE: "Simplify how your team works today.",
+  }
+}
